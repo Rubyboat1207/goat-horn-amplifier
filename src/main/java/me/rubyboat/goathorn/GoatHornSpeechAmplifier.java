143f16be64bec6ef3228d319c64f63dffdb312bc
Rubyboat <52091447+Rubyboat1207@users.noreply.github.com>
2025-01-11 17:05:31 -0600

    Initial Commit

--- src/main/java/me/rubyboat/goathorn/GoatHornSpeechAmplifier.java
+++ src/main/java/me/rubyboat/goathorn/GoatHornSpeechAmplifier.java
@@ -5,14 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GoatHornSpeechAmplifier implements ModInitializer {
 	public static final GameRules.Key<GameRules.IntRule> GOAT_HORN_AMPLIFIER =
 			GameRuleRegistry.register("goatHornAmplifier", GameRules.Category.PLAYER, GameRuleFactory.createIntRule(3, 1, Integer.MAX_VALUE));
 
 	@Override
-	public void onInitialize() {
-	}
+	public void onInitialize() { }
 }
